dfd7a5d0a3505232986552b4ddf4a7509e2a5c0e
birhanberk <birhanberk@gmail.com>
2021-06-17 14:34:37 +0300

    Update pe07.py

--- pe07.py
+++ pe07.py
@@ -1,3 +1,8 @@
+# Problem 7 10001st prime
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 def isprime(a):
   # bir sayının asal olup olmadığını kontrol eden fonksiyon
   i = 3
