c663cd2e9d950bdaae0379e40bab4cd266f39cd0
birhanberk <birhanberk@gmail.com>
2021-06-17 14:59:05 +0300

    Update pe13.py

--- pe13.py
+++ pe13.py
@@ -1,10 +1,14 @@
+# Problem 13 Large sum
+
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
 with open("deneme.txt","r") as dosya:
     liste = dosya.readlines()
-    toplam=0
-    dizi=[]
-    for i in range(0,100):
-        toplam+=int(liste[i])
+    toplam = 0
+    dizi = []
+    for i in range(0, 100):
+        toplam += int(liste[i])
     for k in str(toplam):
-        if(len(dizi)<10):
+        if(len(dizi) < 10):
             dizi.append(int(k))
     print(dizi)
