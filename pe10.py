701ec92f32e63a1edd9248d62e44c50754055c7f
birhanberk <birhanberk@gmail.com>
2021-06-17 14:39:12 +0300

    Update pe10.py

--- pe10.py
+++ pe10.py
@@ -4,13 +4,14 @@
 # Find the sum of all the primes below two million.
 
 def allprimes(n):
-    primes=[]
-    for i in range(2,n+1):
+    # bir sayıdan küçük bütün asal sayıları dizi olarak döndüren fonksiyon
+    primes = []
+    for i in range(2, n + 1):
         primes.append(i)
-    for x in range(0,int(n/2)+1):
-        if(primes[x]!=0):
-            for i in range(x+primes[x],n-1,primes[x]):
-                primes[i]=0
+    for x in range(0, int(n / 2) + 1):
+        if(primes[x] != 0):
+            for i in range(x + primes[x], n - 1, primes[x]):
+                primes[i] = 0
     primes.sort()
     return(primes[primes.count(0):])
   
