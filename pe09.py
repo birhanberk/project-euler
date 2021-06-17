5427c59f6823be43eb9dfc01e5b46af3b2f6a37e
birhanberk <birhanberk@gmail.com>
2021-06-17 14:36:54 +0300

    Update pe09.py

--- pe09.py
+++ pe09.py
@@ -1,3 +1,13 @@
+# Problem 9 Special Pythagorean triplet
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
 for i in range(200,500):
   for j in range(200,500):
     if(i>=j):
@@ -7,5 +17,5 @@ for i in range(200,500):
         continue
       if(i**2+j**2==k**2):
         if(i+j+k==1000):
-          print("Bulduk!!!!!!!----->>",i,j,k)
+          print(i,j,k)
           print(i*j*k)
