f6b6ea2a5a9f085c4f5e8ecdf374fbb72eea70e7
birhanberk <birhanberk@gmail.com>
2021-06-20 11:04:23 +0300

    Update pe17.py

--- pe17.py
+++ pe17.py
@@ -1,26 +1,24 @@
-kitaplik={0:"and",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten",11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen",20:"twenty",30:"thirty",40:"forty",50:"fifty",60:"sixty",70:"seventy",80:"eighty",90:"ninety",100:"hundred",1000:"onethousand"}
+# Problem 17 Number letter counts
+
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+kitaplik = {0:"and", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen", 20:"twenty", 30:"thirty", 40:"forty", 50:"fifty", 60:"sixty", 70:"seventy", 80:"eighty", 90:"ninety", 100:"hundred", 1000:"onethousand"}
 
 def hesapla(i):
-  if(i < 20):
-    #print(i,len(kitaplik[i]))
-    return len(kitaplik[i])
-  if(i >= 20 and i % 10 == 0 and i < 100):
-    #print(i,len(kitaplik[i]))
+  if(i < 20 or i % 10 == 0 and i < 100):
     return len(kitaplik[i])
   if(i < 100):
-    #print(i,hesapla(i - int(i % 10)) + hesapla(i % 10))
-    return hesapla(i - int(i % 10)) + hesapla(i % 10)
+    return hesapla(i - i % 10) + hesapla(i % 10)
   if(i % 100 == 0 and i < 1000):
-    #print(i,hesapla(int(i/100)) + len(kitaplik[100]))
-    return hesapla(int(i/100)) + len(kitaplik[100])
+    return hesapla(i / 100) + len(kitaplik[100])
   if(i < 1000):
-    #print(i,hesapla(i-(i%100)) + len(kitaplik[0]) + hesapla(i%100))
-    return hesapla(i-(i%100)) + len(kitaplik[0]) + hesapla(i%100)
+    return hesapla(i - (i % 100)) + len(kitaplik[0]) + hesapla(i % 100)
   if(i == 1000):
     return len(kitaplik[1000])
 
 sonuc = 0
-
 for i in range(1, 1001):
   sonuc += hesapla(i)
 
