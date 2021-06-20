120a4da51a04876818f2ebd085bdc159cc406046
birhanberk <birhanberk@gmail.com>
2021-06-20 10:50:34 +0300

    Update pe15.py

--- pe15.py
+++ pe15.py
@@ -1,7 +1,12 @@
+# Problem 15 Lattice paths
+
+# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through a 20×20 grid?
+
 def faktoriyel(sayi):
-  if(sayi==1):
+  if(sayi == 1):
     return 1
-  return sayi * faktoriyel(sayi-1)
+  return sayi * faktoriyel(sayi - 1)
 
 sonuc = faktoriyel(40) / (faktoriyel(20) * faktoriyel(20))
-print(sonuc)
+print(int(sonuc))
