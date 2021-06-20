d01b07b080e791e59339c4e9e1d409878f8fa6c1
birhanberk <birhanberk@gmail.com>
2021-06-20 11:11:24 +0300

    Update pe20.py

--- pe20.py
+++ pe20.py
@@ -1,6 +1,17 @@
-import math
-toplam=0
-x=math.factorial(100)
+# Problem 20 Factorial digit sum
+
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# Find the sum of the digits in the number 100!
+
+def faktoriyel(sayi):
+  if(sayi == 1):
+    return 1
+  return sayi * faktoriyel(sayi - 1)
+
+toplam = 0
+x = faktoriyel(100)
 for i in str(x):
-  toplam+=int(i)
+  toplam += int(i)
 print(toplam)
