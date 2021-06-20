00597f8f2fcc7f20bbead85782ea78b4bd93a69c
birhanberk <birhanberk@gmail.com>
2021-06-20 10:54:25 +0300

    Update pe16.py

--- pe16.py
+++ pe16.py
@@ -1,6 +1,10 @@
-import math
-toplam=0
-kuvveti=int(math.pow(2,1000))
+# Problem 16 Power digit sum
+
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+
+toplam = 0
+kuvveti = 2 ** 1000
 for i in str(kuvveti):
-  toplam=toplam+int(i)
+  toplam = toplam + int(i)
 print(toplam)
