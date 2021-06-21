518b4012fe6d1ba2c79b766bd451df13e6dcd1c3
birhanberk <birhanberk@gmail.com>
2021-06-21 14:14:42 +0300

    Update pe36.py

--- pe36.py
+++ pe36.py
@@ -1,23 +1,25 @@
-toplam=0
-for i in range(1,2000000):
-  liste=[]
-  x=[]
+# Problem 36 Double-base palindromes
+
+# The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+# (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+def palindromMu(dizi):
+  if(dizi == dizi[::-1]):
+    return True
+  return False
+
+toplam = 0
+for i in range(1, 1000000):
+  dizi = []
   for j in str(i):
-    liste.append(j)
-    x.append(j)
-  liste.reverse()
-  y=liste
-  if(x==y):
-    ikili=[]
-    a=[]
-    h=i
-    while h>0:
-      ikili.append(h%2)
-      a.append(h%2)
-      h=int(h/2)
-    ikili.reverse()
-    b=ikili
-    if(a==b):
-      print(i,a)
-      toplam+=i
+    dizi.append(j)
+  if(palindromMu(dizi)):
+    ikili = []
+    h = i
+    while h > 0:
+      ikili.append(h % 2)
+      h = int(h / 2)
+    if(palindromMu(ikili)):
+      toplam += i
 print(toplam)
