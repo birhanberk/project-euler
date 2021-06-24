0f06ffe094153c8e168b2d0fe9b541dc7daa9970
birhanberk <birhanberk@gmail.com>
2021-06-24 20:49:15 +0300

    Update pe34.py

--- pe34.py
+++ pe34.py
@@ -1,3 +1,9 @@
+# Problem 34 Digit factorials
+
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+# Note: As 1! = 1 and 2! = 2 are not sums they are not included.
+
 def faktoriyel(n):
   toplam = 1
   for a in range(1, n + 1):
