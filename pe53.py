e9a167b71df80b50b445c92a6cd3c8ee4da9af0b
birhanberk <birhanberk@gmail.com>
2021-06-24 20:53:06 +0300

    Update pe53.py

--- pe53.py
+++ pe53.py
@@ -1,16 +1,20 @@
-def faktoriyel(sayi):
-  fak=1
-  for i in range(1,sayi+1):
-    fak=fak*i
-  return(fak)
+# Problem 53 Combinatoric selections
 
-def kombinasyon(n,r):
-  kom=faktoriyel(n)//(faktoriyel(r)*(faktoriyel(n-r)))
-  return(kom)
-sayac=0
-for i in range(1,101):
-  for j in range(1,i+1):
-    a=kombinasyon(i,j)
-    if(a>1000000):
-      sayac+=1
+# https://projecteuler.net/problem=53
+
+def faktoriyel(n):
+  toplam = 1
+  for a in range(1, n + 1):
+    toplam *= a;
+  return toplam
+
+def kombinasyon(n, r):
+  return faktoriyel(n) // (faktoriyel(r) * (faktoriyel(n - r)))
+
+sayac = 0
+for i in range(1, 101):
+  for j in range(1, i + 1):
+    a = kombinasyon(i, j)
+    if(a > 1000000):
+      sayac += 1
 print(sayac)
